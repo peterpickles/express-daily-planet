939c49d4613f3c197636be254a54642d2628a27a
peterpickles <lipeter2006@gmail.com>
2018-01-29 06:18:02 -0800

    worked out the issue with the error

--- controllers/articles.js
+++ controllers/articles.js
@@ -1,1 +1,20 @@
-articles.js
+var express = require("express");
+var router = express.Router();
+db = require("../models");
+var app = express();
+
+router.get("/", function(req,res){
+	db.dailyplanet.findAll().then(function(createdArticles){
+		res.render("article/index", {results: createdArticles});
+	});
+});
+
+router.get("/:id", function(req, res){
+	db.dailyplanet.findById(req.params.id).then(function(article){
+		res.render("articles/show", {result: article});
+	});
+});
+
+// app.post 
+
+module.export = router;
--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var bodyParser = require('body-parser');
-var ejsLayouts = require('express-ejs-Layouts')
+var ejsLayouts = require('express-ejs-Layouts');
 var app = express();
 
 app.set('view engine', 'ejs');
+
+//middleware
 app.use(ejsLayouts);
 app.use(bodyParser.urlencoded({extended: false}));
 
@@ -12,15 +14,20 @@ app.get('/', function(req,res){
 	res.render("./site/home.ejs");
 });
 
+app.get("/articles", function(req, res){
+	// res.send("this is my articles page");
+	res.render("./article/index.ejs");
+});
+
 app.get("/contact", function(req, res){
-	res.send("this is my contacts page");
-	// res.render("contact");
+	// res.send("this is my contacts page");
+	res.render("./site/contact");
 });
 
 app.get("/about", function(req, res){
-	res.send("this is my abouts page");
-	// res.render("about");
+	// res.send("this is my abouts page");
+	res.render("./site/about.ejs");
 });
 
-
+app.use("/articles", require("./controllers/articles"));
 app.listen(3000);
